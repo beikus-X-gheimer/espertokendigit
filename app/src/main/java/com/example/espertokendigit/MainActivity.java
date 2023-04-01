b119d584283ab20613d59501cdf9531052c9f19b
beikus-X-gheimer <118048165+beikus-X-gheimer@users.noreply.github.com>
2023-04-01 10:24:46 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/espertokendigit/MainActivity.java
+++ app/src/main/java/com/example/espertokendigit/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.espertokendigit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//commento
 public class MainActivity extends AppCompatActivity {
 
     @Override
